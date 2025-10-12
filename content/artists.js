eae9bbafbfccfe44812c198d6a35f83998dd727f
bellosoundsrecords <emanuelesiravo@gmail.com>
2025-10-12 11:50:12 +0200

    artists.js

--- content/artists.js
+++ content/artists.js
@@ -5,7 +5,7 @@ export const artists = [
     name: "Nick Evan",
     tagline: "Identity, Evolution. <br> Sound comes first",
     bioShort: "Nick Evan is a sound project born inside BelloSounds Records. It comes from a time—the ’90s and 2000s—that still moves us. It starts again from there: no imitation, it remembers where it came from and looks ahead. No special effects—just presence. Memory becoming future.",
-    bioLong: "Nick Evan is a sound project born inside BelloSounds Records. It comes from a time—the ’90s and 2000s—that still moves us. It starts again from there: it doesn’t imitate; it remembers and looks forward, to preserve and pass on that culture. <br>The music begins with a simple idea: a few well-chosen elements and room to breathe. It changes the air through details, not special effects: a step, a pause, a chord that opens. Deep house with urban and soulful touches, made to move without shouting—from the club to the headphones. <br>Memory becoming future.",
+    bioLong: "Nick Evan is a sound project born inside BelloSounds Records. It comes from a time—the ’90s and 2000s—that still moves us. It starts again from there: it doesn’t imitate; it remembers and looks forward, to preserve and pass on that culture. <br>His music begins with a simple idea: a few well-chosen elements and room to breathe. It changes the air through details, not special effects: a step, a pause, a chord that opens. Deep house with urban and soulful touches, made to move without shouting—from the club to the headphones. <br>Memory becoming future.",
     image: "./images/artists/Nick-Evan.jpg",
     socials: {
       youtube: "https://youtube.com/@Nick_Evan",
