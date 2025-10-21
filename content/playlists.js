b80aa568d7d997ac38656a9d54ddde899855fe67
bellosoundsrecords <emanuelesiravo@gmail.com>
2025-10-21 10:17:18 +0200

    Add platform information to playlists

--- content/playlists.js
+++ content/playlists.js
@@ -5,6 +5,7 @@ export const playlists = [
     title: "Release 2025",
     description: "All BelloSounds Records releases for 2025. Two sides, one vision: Nick Evan & Neel Miles. Deep, soulful, raw.",
     cover: "./images/playlists/release-2025.jpg",
+    platform: "YouTube",
     embeds: "https://www.youtube.com/watch?v=-SMEW-rV2S4&list=PLdAdqW9kOD61wSxWqIFd_7dK49z4Rwq3V",
     items: ["bsr004-lost-in-the-feeling", "bsr003-night-at-district", "bsr002-city-tension", "bsr001-your-heart"]
   }
