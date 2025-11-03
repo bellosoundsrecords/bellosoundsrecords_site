768d2d32f1f94624cd124ef160b62a73b012dc63
bellosoundsrecords <emanuelesiravo@gmail.com>
2025-11-03 23:10:47 +0100

    Update brand name to use trademark symbol

--- content/settings.js
+++ content/settings.js
@@ -1,6 +1,6 @@
 // content/settings.js
 export const settings = {
-  brand: "BelloSounds Records&trade;",
+  brand: "BelloSounds Records™",
   tagline: "Two sides, one vision.",
   hashtags: ["#deephouse", "#soulfulhouse", "#deepgarage", "#bellosoundsrecords"],
   socials: {
