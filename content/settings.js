6c041ba6a4ba7e4a1d3546ede8e25d1a28cce686
bellosoundsrecords <emanuelesiravo@gmail.com>
2026-07-15 23:19:33 +0200

    Update Spotify link in settings.js

--- content/settings.js
+++ content/settings.js
@@ -6,7 +6,7 @@ export const settings = {
   socials: {
     youtube: "https://youtube.com/@BelloSoundsRecords",
     soundcloud:"https://soundcloud.com/bellosoundsrecords",
-    spotify: "https://open.spotify.com/",
+    spotify: "https://open.spotify.com/user/31ammax2ahod5upq7lcxur7hgwfm?si=35d76c4ae64a484f",
     discogs: "https://www.discogs.com/label/4322125-BelloSounds-Records"
   }
 };
